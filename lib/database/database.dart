be916cfc1db791a4370d04726c2029bff86c37cf
Bert Lin <76031581+smtp10185@users.noreply.github.com>
2023-09-26 14:40:37 +0800

    remove useless code

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -36,51 +36,6 @@ class AppDatabase extends _$AppDatabase {
         .getSingleOrNull();
   }
 
-  // Instructor
-  Future<List<Instructor>> getAllInstructors() => select(instructors).get();
-  Stream<List<Instructor>> watchAllInstructors() => select(instructors).watch();
-  Future<int> insertInstructor(Insertable<Instructor> instructor) =>
-      into(instructors).insert(instructor);
-  Future<bool> updateInstructor(Insertable<Instructor> instructor) =>
-      update(instructors).replace(instructor);
-  Future<int> deleteInstructor(Insertable<Instructor> instructor) =>
-      delete(instructors).delete(instructor);
-
-  // Student
-  Future<List<Student>> getAllStudents() => select(students).get();
-  Stream<List<Student>> watchAllStudents() => select(students).watch();
-  Future<int> insertStudent(Insertable<Student> student) =>
-      into(students).insert(student);
-  Future<bool> updateStudent(Insertable<Student> student) =>
-      update(students).replace(student);
-  Future<int> deleteStudent(Insertable<Student> student) =>
-      delete(students).delete(student);
-
-  // Course
-  Future<List<Course>> getAllCourses() => select(courses).get();
-  Stream<List<Course>> watchAllCourses() => select(courses).watch();
-  Future<int> insertCourse(Insertable<Course> course) =>
-      into(courses).insert(course);
-  Future<bool> updateCourse(Insertable<Course> course) =>
-      update(courses).replace(course);
-  Future<int> deleteCourse(Insertable<Course> course) =>
-      delete(courses).delete(course);
-
-  // CourseRegistration
-  Future<List<CourseRegistration>> getAllCourseRegistrations() =>
-      select(courseRegistrations).get();
-  Stream<List<CourseRegistration>> watchAllCourseRegistrations() =>
-      select(courseRegistrations).watch();
-  Future<int> insertCourseRegistration(
-          Insertable<CourseRegistration> registration) =>
-      into(courseRegistrations).insert(registration);
-  Future<bool> updateCourseRegistration(
-          Insertable<CourseRegistration> registration) =>
-      update(courseRegistrations).replace(registration);
-  Future<int> deleteCourseRegistration(
-          Insertable<CourseRegistration> registration) =>
-      delete(courseRegistrations).delete(registration);
-
   onCreate() {}
 }
 
